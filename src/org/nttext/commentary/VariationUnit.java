8ee6db947c3bc58d44d67dcf1ab6bbc0f7b007cf
neal <devnull@localhost>
2011-09-01 15:01:00 -0500

    Minor revisions.

--- src/org/nttext/commentary/VariationUnit.java
+++ src/org/nttext/commentary/VariationUnit.java
@@ -3,11 +3,24 @@
  */
 package org.nttext.commentary;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Neal Audenaert
  */
 public class VariationUnit {
 	
-	
+	private Long id;
+	private String commentary;
+	private List<VariantReading> readings;
 
+	
+	public List<VariantReading> getReadings() {
+		return Collections.unmodifiableList(this.readings);
+	}
+	
+	public void addReading(VariantReading rdg) {
+		
+	}
 }

b41161f5743b284eb454792bac982184f1a4ac38
neal <devnull@localhost>
2011-09-27 10:11:42 -0500

    Added support for general purpose properties to the context.

--- src/openscriptures/text/importer/Context.java
+++ src/openscriptures/text/importer/Context.java
@@ -19,6 +19,8 @@ public class Context {
     
     private Map<String, Boolean> flags = new HashMap<String, Boolean>();
     
+    private Map<String, String> props = new HashMap<String, String>();
+    
     /** The <tt>Work</tt> that is being imported. */
     public MutableWork work = null;
     
@@ -36,6 +38,61 @@ public class Context {
  
     public int currentVerse = 0;
     public int currentChapter = 0;
+    
+    /**
+     * Sets a numeric property of the context.
+     *  
+     * @param key The key for the property to set.
+     * @param value The value to be set.
+     * 
+     * @return The previous value for this property.
+     */
+    public String set(String key, double value) {
+        return this.props.put(key, Double.toString(value));
+    }
+
+    /**
+     * Sets a string-valued property of this context.
+     * 
+     * @param key The key for the property to set.
+     * @param value The value to be set
+     * 
+     * @return The previous value for this property.
+     */
+    public String set(String key, String value) {
+        return this.props.put(key, value);
+    }
+    
+    /**
+     * Returns the value of a string-valued property.
+     * @param key The property whose value should be returned.
+     * 
+     * @return 
+     */
+    public String get(String key) {
+        return this.props.get(key);
+    }
+    
+    /**
+     * Returns the value of a numeric property.  
+     * 
+     * @param key The property whose value should be returned
+     * @return The set value or <tt>Double.NaN</tt> if the set value is null.
+     */
+    public double getn(String key) {
+        double d = Double.NaN;
+        String value = this.props.get(key);
+        if (value != null) {
+            try {
+                d = Double.parseDouble(value);
+            } catch (NumberFormatException nfe) {
+                // TODO log warning
+                d = Double.NaN;
+            }
+        }
+        
+        return d;
+    }
     
     public void flag(String state, Boolean value) {
         flags.put(state, value);

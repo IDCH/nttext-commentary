a47b00642c7a868432d584880637397f6a231c91
neal <devnull@localhost>
2011-10-04 14:06:19 -0500

    Initial implementation of the Catalog repository class.

--- src/org/nttext/mss/Catalog.java
+++ src/org/nttext/mss/Catalog.java
@@ -6,21 +6,10 @@ package org.nttext.mss;
 /**
  * @author Neal Audenaert
  */
-public class Catalog {
-
+public interface Catalog {
+    public Manuscript createManuscript(String scheme, String identifier);
     
-    /**
-     * Finds the manuscript corresponding to a specific designation.
-     * 
-     * @param designation The designation of the manuscript to find.
-     * @return The designated manuscript or null if no such manuscript exists.
-     */
-	public Manuscript find(Designation designation) {
-	    return null;
-	}
-	
-	public Designation createMSDesignation(String scheme, String id) {
-	    Designation d = new Designation(scheme, id);
-	    return d;
-	}
+    public Manuscript lookup(Designation designation);
+    
+    public Designation createMSDesignation(String scheme, String id);
 }

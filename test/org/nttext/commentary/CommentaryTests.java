71e0a958224fb3bda37b5bfc41556adc2591d23c
neal <devnull@localhost>
2011-10-24 14:59:29 -0500

    Added initial test cases for the commentary.

---
+++ test/org/nttext/commentary/CommentaryTests.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.nttext.commentary;
+
+import org.idch.persist.RepositoryAccessException;
+import org.nttext.commentary.persist.MySQLCommentaryRepo;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Neal Audenaert
+ */
+public class CommentaryTests extends TestCase {
+    
+    MySQLCommentaryRepo repo = null;
+    
+    public void setUp() {
+        try {
+            repo = MySQLCommentaryRepo.get();
+        } catch (RepositoryAccessException e) {
+            repo = null;
+            e.printStackTrace();
+        }
+    }
+    
+    public void tearDown() {
+        try {
+            repo.drop();
+        } catch (RepositoryAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void testDBCreate() throws RepositoryAccessException {
+        if (repo.probe()) 
+            repo.drop();
+        
+        repo.create();
+    }
+
+}
